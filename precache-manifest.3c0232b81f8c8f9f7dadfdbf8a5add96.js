7d94d586b949903604ff01f2d18b3c3fdc804a9d
reemnx <37637982+reemnx@users.noreply.github.com>
2020-07-29 17:03:35 +0300

    Updates

---
+++ precache-manifest.3c0232b81f8c8f9f7dadfdbf8a5add96.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443cad307d5e104ff8409070b066a406",
+    "url": "/guardio_proj/index.html"
+  },
+  {
+    "revision": "9bbc1b9bfc7879e7edb6",
+    "url": "/guardio_proj/static/css/main.ffe658e4.chunk.css"
+  },
+  {
+    "revision": "cb6eec3d8066f10f451a",
+    "url": "/guardio_proj/static/js/2.8c31248d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/guardio_proj/static/js/2.8c31248d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bbc1b9bfc7879e7edb6",
+    "url": "/guardio_proj/static/js/main.2234d4d5.chunk.js"
+  },
+  {
+    "revision": "d0ac72a8dcba1db82306",
+    "url": "/guardio_proj/static/js/runtime-main.f58f7700.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/guardio_proj/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "683362f36187ad8be18692df9c1cf81e",
+    "url": "/guardio_proj/static/media/Muli-Medium.683362f3.ttf"
+  },
+  {
+    "revision": "d890272164b90fefd201d535351e3317",
+    "url": "/guardio_proj/static/media/galaxy2.d8902721.jpg"
+  },
+  {
+    "revision": "bec33bcea8492d6a4fdc65ae430dddc7",
+    "url": "/guardio_proj/static/media/next.bec33bce.svg"
+  },
+  {
+    "revision": "19bda207d69c7d53209a23ade481c919",
+    "url": "/guardio_proj/static/media/rainbow.19bda207.jpg"
+  },
+  {
+    "revision": "114085d5832bb2ecb23cb7d638f8a992",
+    "url": "/guardio_proj/static/media/send.114085d5.svg"
+  }
+]);
