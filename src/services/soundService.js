ab71da5f2c5f92e1db36d2717df4907369c448b9
reemnx <37637982+reemnx@users.noreply.github.com>
2020-07-29 17:41:58 +0300

    deploy

--- src/services/soundService.js
+++
@@ -1,9 +0,0 @@
-export default {
-    notification
-}
-
-function notification(){
-    var notifiPing = new Audio('https://res.cloudinary.com/dt3cfrjpr/video/upload/v1590757355/messenger_d1vdx5.mp3');
-    notifiPing.volume = 0.15;
-    notifiPing.play();
-}
